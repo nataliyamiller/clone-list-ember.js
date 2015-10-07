3442ba0f1777520f71cb745a78c7f7b3f9d7b833
nataliyamiller <nataliyamiller82@gmail.com>
2015-10-06 20:36:41 -0700

    Adds delete functionality to a single listing page

---
+++ app/components/listing-page.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    imageShow: function() {
+      this.set('isImageShowing', true);
+    },
+    imageHide: function() {
+      this.set('isImageShowing', false);
+    },
+
+    deleteListing(listing) {
+      if (confirm('Are you sure you want to delete this listing?')) {
+        this.sendAction('deleteListing', listing);
+      }
+    }
+  }
+});
