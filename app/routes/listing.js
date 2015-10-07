341ec07d859e1ae77bb3fc31147c5b71e8934eea
nataliyamiller <nataliyamiller82@gmail.com>
2015-10-06 21:31:37 -0700

    Some styling for the listing page

--- app/routes/listing.js
+++ app/routes/listing.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('listing', params.listing_id);
   },
-  
+
   actions: {
     deleteListing(listing) {
       listing.destroyRecord().then(function() {
